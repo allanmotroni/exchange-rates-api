655faaefc359c7b4974200f5343976ba1318cbe0
Allan Motroni <allan.motroni@hotmail.com>
2021-05-29 23:17:35 -0300

    Creating new Transaction DTO and new User DTO

---
+++ ExchangeRates.Domain/Dto/NewUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.Domain.Dto
+{
+    public class NewUserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} - Email: {Email}";
+        }
+    }
+}
