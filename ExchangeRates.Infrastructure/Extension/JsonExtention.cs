c23822b6d2fb5aacc3a4827c299200a2bf1dfa24
Allan Motroni <allan.motroni@hotmail.com>
2021-05-30 20:42:52 -0300

    Creating extension method to parse json to defined class.

---
+++ ExchangeRates.Infrastructure/Extension/JsonExtention.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace ExchangeRates.Infrastructure.Extension
+{
+    public static class JsonExtention
+    {
+        public static T ToClassOf<T>(this string json)
+        {
+            if (!string.IsNullOrEmpty(json))
+                return JsonSerializer.Deserialize<T>(json);
+
+            return default;
+        }
+    }
+}
