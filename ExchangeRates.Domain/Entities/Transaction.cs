07b4847786687a3161f23db99790a1c2fe730744
Allan Motroni <allan.motroni@hotmail.com>
2021-05-29 22:06:45 -0300

    Creating Transaction and User entities

---
+++ ExchangeRates.Domain/Entities/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.Domain.Entities
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string FromCurrency { get; set; }
+        public double FromValue { get; set; }
+        public double ToCurrency { get; set; }
+        public double ToValue { get; set; }
+        public double Rate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
