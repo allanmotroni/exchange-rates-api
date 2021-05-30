dcee862f2eb825ed2b1c9fbc8eef6b19401eb0e4
Allan Motroni <allan.motroni@hotmail.com>
2021-05-30 17:21:53 -0300

    Creating console log library.

---
+++ ExchangeRates.Domain/Interfaces/Logger/ICustomLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExchangeRates.Domain.Interfaces.Logger
+{
+    public interface ICustomLogger
+    {
+        public void Info(string message);
+
+        public void Warn(string message);
+
+        public void Error(string message);
+
+        public void Exception(string message, Exception exception);
+
+        public void Exception(Exception exception);
+
+    }
+}
---
+++ ExchangeRates.Infrastructure/Logger/CustomLogger.cs
@@ -0,0 +1,48 @@
+﻿using ExchangeRates.Domain.Interfaces.Logger;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ExchangeRates.Infrastructure.Logger
+{
+
+    public class CustomLogger : ICustomLogger
+    {
+        private readonly ILogger<CustomLogger> _logger;
+        public CustomLogger(ILogger<CustomLogger> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Info(string message)
+        {
+            _logger.LogInformation($"{BaseInformation()} - {message}");
+        }
+
+        public void Warn(string message)
+        {
+            _logger.LogWarning($"{BaseInformation()} - {message}");
+        }
+
+        public void Error(string message)
+        {
+            _logger.LogError($"{BaseInformation()} - {message}");
+        }
+
+        public void Exception(Exception exception)
+        {
+            _logger.LogCritical(exception, $"{BaseInformation()}");
+        }
+
+        public void Exception(string message, Exception exception)
+        {
+            _logger.LogCritical(exception, $"{BaseInformation()} - {message}");
+        }
+
+        private string BaseInformation()
+        {
+            string method = (new System.Diagnostics.StackTrace()).GetFrame(1).GetMethod().Name;
+
+            return $"Date: {DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss")} - Method: {method}";
+        }
+    }
+}
