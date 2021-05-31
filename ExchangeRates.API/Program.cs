ba81d3b2821ccaea05793af8c2dd8adc5e1565fb
Allan Motroni <allan.motroni@hotmail.com>
2021-05-30 21:29:44 -0300

    Changing dockerfile to deploy on Heroku

--- ExchangeRates.API/Program.cs
+++ ExchangeRates.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ExchangeRates.API
 {
@@ -15,6 +16,7 @@ namespace ExchangeRates.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
