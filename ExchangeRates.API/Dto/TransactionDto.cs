ec6ff05472aeac696b95a0962162e0f902cad280
Allan Motroni <allan.motroni@hotmail.com>
2021-05-30 21:16:20 -0300

    Changing Transaction dto to include TransactionId and ToValue.

--- ExchangeRates.API/Dto/TransactionDto.cs
+++ ExchangeRates.API/Dto/TransactionDto.cs
@@ -4,16 +4,19 @@ namespace ExchangeRates.API
 {
     public class TransactionDto
     {
+        public int TransactionId { get; set; }
         public int UserId { get; set; }
         public string FromCurrency { get; set; }
         public double FromValue { get; set; }
         public string ToCurrency { get; set; }
+        public double ToValue { get; set; }
+
         public double Rate { get; set; }
         public DateTime CreatedAt { get; set; }
 
         public override string ToString()
         {
-            return $"UserId: {UserId} - FromCurrency: {FromCurrency} - FromValue: {FromValue} - ToCurrency: {ToCurrency} - Rate: {Rate} - CreatedAt: {CreatedAt}";
+            return $"Id: {TransactionId} - UserId: {UserId} - FromCurrency: {FromCurrency} - FromValue: {FromValue} - ToCurrency: {ToCurrency} - ToValue: {ToValue} - Rate: {Rate} - CreatedAt: {CreatedAt}";
         }
     }
 }
