adea1c3d954770120027d939b0e5575d1a8f3fa1
Allan Motroni <amotroni@lopes.com.br>
2021-06-07 19:44:31 -0300

    Removing unusing usings from class.

--- ExchangeRates.Test/Infrastructure/Repositories/TransactionRepositoryTest.cs
+++ ExchangeRates.Test/Infrastructure/Repositories/TransactionRepositoryTest.cs
@@ -7,8 +7,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ExchangeRates.Test.Infrastructure.Repositories
